7f6f1f856f4185eba8bdece1bd0f96d0bce8f148
Emmett Moore <emmett.moore823@gmail.com>
2022-12-15 19:35:54 -0500

    day 12 p 2

--- @emmettmoore/advent-of-code/src/solutions/day12/part2/part2.ts
+++ @emmettmoore/advent-of-code/src/solutions/day12/part2/part2.ts
@@ -1,5 +1,42 @@
-import { NotImplementedError } from '../../../errors';
+import {
+  Position,
+  findPosition,
+  subStartAndEnd,
+  NoPathToSummitError,
+  findShortestPath,
+} from '../utils';
 
-export default async (): Promise<string> => {
-  throw new NotImplementedError(`Unsolved`);
+import getData from '../getData';
+
+export default async (): Promise<number> => {
+  const data = getData();
+
+  const endPosition = findPosition(data, `E`);
+
+  const grid = subStartAndEnd(data);
+
+  const allAs = new Array<Position>();
+
+  grid.forEach((row, y) => {
+    row.forEach((letter, x) => {
+      if (letter === `a`) {
+        allAs.push({ x, y });
+      }
+    });
+  });
+
+  const shortestPaths = new Array<number>();
+  allAs.forEach((a) => {
+    try {
+      const shortestPath = findShortestPath(grid, a, endPosition);
+      shortestPaths.push(shortestPath);
+    } catch (err) {
+      if (err instanceof NoPathToSummitError) {
+        return;
+      }
+      throw err;
+    }
+  });
+
+  return Math.min(...shortestPaths);
 };
