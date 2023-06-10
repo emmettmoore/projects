f1d6cf62094e914faed2e2f21c67b767349d1077
Emmett Moore <emmett.moore823@gmail.com>
2023-06-10 16:26:54 -0400

    midpoint

---
+++ @emmettmoore/site/next.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable filenames/match-exported */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
