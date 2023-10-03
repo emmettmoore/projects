b78c11b2dfc1af288af618a635d23c97b188d98d
Emmett Moore <emmett.moore823@gmail.com>
2023-10-03 19:12:54 -0400

    midpoint

--- @emmettmoore/site/src/common/externalLinks.ts
+++ @emmettmoore/site/src/common/externalLinks.ts
@@ -2,6 +2,6 @@ export const EMMETT_GITHUB_URL = `https://github.com/emmettmoore`;
 export const EMMETT_LINKEDIN_URL = `https://www.linkedin.com/in/emmett-moore/`;
 
 export const EMMETT_GH_PROJECTS_URL = `https://www.github.com/emmettmoore/projects`;
-export const PERSONAL_SITE_URL = `https://www.github.com/emmettmoore/projects/@emmettmoore/site`;
+export const PERSONAL_SITE_URL = `https://www.github.com/emmettmoore/projects/tree/main/@emmettmoore/site`;
 
 export const PERSONAL_SITE_OLD_URL = `https://web.archive.org/web/20211130202131/http://www.emmettwmoore.com/`;
