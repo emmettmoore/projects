00bf6908fcd4afd8c7920f138583ae2326523faf
Emmett Moore <emmett.moore823@gmail.com>
2023-10-04 11:42:32 -0400

    midpoint

--- @emmettmoore/site/src/theme/mui/palette.ts
+++ @emmettmoore/site/src/theme/mui/palette.ts
@@ -1,16 +1,18 @@
+// todo - codegen palette -> colors.scss
+
 export default {
   // https://maketintsandshades.com/#080047 20% values
   primary: {
-    light: `#dc7074`,
+    light: `#5581a5`,
     main: `#2A628F`,
-    dark: `#a93d41`,
+    dark: `#224e72`,
   },
 
   // https://maketintsandshades.com/#453384 20% values
   secondary: {
-    light: `#6a5c9d`,
-    main: `#DFAE1C`,
-    dark: `#37296a`,
+    light: `#e5be42`,
+    main: `#dfae13`,
+    dark: `#b28b0f`,
   },
   common: {
     black: `#000`,
