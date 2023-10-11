aa7d2df3c4bd6c022218161948f75edb94d82f06
Emmett Moore <Emmett.Moore823@gmail.com>
2023-10-11 11:30:49 -0400

    Update Picture (#16)
    This commit just updates the picture of me on the home page to a new one

--- @emmettmoore/site/src/containers/HomePage/components/SelfImage/SelfImage.tsx
+++ @emmettmoore/site/src/containers/HomePage/components/SelfImage/SelfImage.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable filenames/match-exported */
 import Image from 'next/image';
-import { EMMETT_AND_ROCKY } from '@site/common/static';
+import { EMMETT_GREEN_CLIFF } from '@site/common/static';
 
 import styles from './SelfImage.module.scss';
 
 const SelfImage = (): JSX.Element => {
   return (
     <Image
-      alt="Me and my cat"
+      alt="Picture of Emmett climbing"
       className={styles.image}
       height={427}
-      src={EMMETT_AND_ROCKY}
+      src={EMMETT_GREEN_CLIFF}
       width={320}
     />
   );
